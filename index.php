a8c3480a5c9600982d87978560a1942ac2b02960
JhonnGB <jhondguevara@gamil.com>
2024-10-25 17:34:23 -0500

    Primeros estilos CSS

--- index.php
+++ index.php
@@ -4,58 +4,67 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./css/index.css?v=1.0">
+    <title>Register</title>
 </head>
 
 <body>
+    <main class="contenedor">
+        <h1 class="titulo">FORMULARIO</h1>
+        <hr>
 
-    <?php
-     $use = 'caramelo';
-    ?>
-    <h1>FORMULARIO</h1>
+        <div class="contenedor_form">
+            <form class="formulario" action="">
+                <h2 class="subtitulo">Datos Persona</h2>
 
-    <form action="">
-        <div>
-            <label for="">Nombre</label>
-            <input type="text">
-        </div>
-        <div>
-            <label for="">Apellido</label>
-            <input type="text">
-        </div>
-        <div>
-            <label for="">Edad</label>
-            <input type="int">
-        </div>
-        <div>
-            <label for="">Sede</label>
-            <select id="sede" placeholder="Select a sede">
-                <option
-                    value="">Seleccionar</option>
-                <option value="1">limonar</option>
-                <option value="2">picaleña</option>
-            </select>
-        </div>
-        <div>
-            <label for="">Area</label>
-            <select id="area" placeholder="Select a area">
-                <option
-                    value="">Seleccionar</option>
-                <option value="1">Administrativa</option>
-                <option value="2">Compras</option>
-                <option value="2">
-                    <?php echo $use; ?>
-                </option>
-            </select>
+                <div class="campos">
+                    <div class="campos_div">
+                        <label class="label" for="nombre">Nombre</label>
+                        <input type="text" id="nombre" name="nombre" required>
+                    </div>
+                    <div class="campos_div">
+                        <label for="apellido">Apellido</label>
+                        <input type="text" id="apellido" name="apellido" required>
+                    </div>
+                    <div class="campos_div">
+                        <label for="edad">Edad</label>
+                        <input type="number" id="edad" name="edad" min="0" required>
+                    </div>
+                </div>
+
+                <hr class="separador">
+
+                <h2 class="subtitulo">Rol Persona</h2>
+                <div class="campos">
+                    <div class="campos_div">
+                        <label for="sede">Sede</label>
+                        <select id="sede" name="sede" required>
+                            <option value="">Seleccionar</option>
+                            <option value="1">limonar</option>
+                            <option value="2">picaleña</option>
+                        </select>
+                    </div>
+                    <div class="campos_div">
+                        <label for="area">Area</label>
+                        <select id="area" name="area" required>
+                            <option value="">Seleccionar</option>
+                            <option value="1">Administrativa</option>
+                            <option value="2">Compras</option>
+                        </select>
+                    </div>
+                </div>
+
+                <hr class="separador">
+
+                <div class="form_buttons">
+                    <button>Reset</button>
+                    <button>Enviar</button>
+                </div>
+            </form>
         </div>
 
 
-        <div>
-            <button>Reset</button>
-            <button>Enviar</button>
-        </div>
-    </form>
-
+    </main>
 </body>
 
 </html>
